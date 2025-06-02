35d2a6f92759499914a4f5e0566ca02063f35ac5
Clement-Robbe <clement.robbe@gmail.com>
2025-06-02 16:48:46 +0200

    123

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,6 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./styles/index.css";
+import { AuthProvider } from "./contexts/authContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+createRoot(document.getElementById("root")).render(
+    <AuthProvider>
+        <App />
+    </AuthProvider>
+);
