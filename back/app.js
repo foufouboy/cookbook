c473a9d4470d03806c136746c22f411f34cd0a8d
ChokoDev <jphilblanchet@yahoo.fr>
2025-06-02 17:35:49 +0200

    Architecture/router created

--- back/app.js
+++ back/app.js
@@ -1,8 +1,10 @@
 // salut
+const userRoute = require("./routes/userRoute.js");
 
 const express = require("express");
 const app = express();
 
+app.use(userRoute);
 app.get("/", (req, res) => res.send("hello"));
 
 const PORT = 3000;
@@ -10,3 +12,5 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log("Salut ^^")
 })
+
+// test
