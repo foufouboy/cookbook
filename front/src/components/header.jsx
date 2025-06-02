ffedc2699c3a4f3a9a8a8113ccf7bfce0be053de
ThomasDev31 <thomas.tissier@3wa.io>
2025-06-02 18:00:22 +0200

    adding pages (front)

--- front/src/components/header.jsx
+++ front/src/components/header.jsx
@@ -4,10 +4,16 @@ function Header() {
     return (
         <>
             <div className="navLink">
-                <NavLink to="/"> Accueil</NavLink>
-                <NavLink to="/Login"> Connexion</NavLink>
-                <NavLink to="/Register"> Enregistrement</NavLink>
-                <NavLink to="/Logout"> Déconnexion</NavLink>
+                <div className="logo">
+                  <NavLink to="/"> <img src="logo.png" /> </NavLink>
+                </div>
+                <div className="links">
+                  <NavLink to="/"> Accueil</NavLink>
+                  <NavLink to="/Login"> Connexion</NavLink>
+                  <NavLink to="/Register"> Enregistrement</NavLink>
+                  <NavLink to="/Logout"> Déconnexion</NavLink>
+
+                </div>
             </div>
         </>
     );
