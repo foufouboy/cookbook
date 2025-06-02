de09338cb24787392aa612b4fb648c2332e9e9a3
Clement-Robbe <clement.robbe@gmail.com>
2025-06-02 18:02:59 +0200

    stp

--- front/src/pages/Recipe.jsx
+++ front/src/pages/Recipe.jsx
@@ -1,7 +1,42 @@
 import "../styles/recipe.sass";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router";
 
 function Recipe() {
-    return <></>;
+    const [recipe, setRecipe] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
+    const { id } = useParams();
+
+    async function fetchRecipe() {
+        try {
+            const response = await fetch(`http://localhost:3000/recipes/${id}`);
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            setRecipe(await response.json());
+        } catch (error) {
+            navigate("/404");
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        if (id) {
+            fetchRecipe(id);
+        }
+        setLoading(false);
+    }, [id]);
+
+    return loading ? (
+        <h1 className="loading">Loading...</h1>
+    ) : (
+        <div className="Recipe">
+            <h1>{recipe.name}</h1>
+        </div>
+    );
 }
 
 export default Recipe;
