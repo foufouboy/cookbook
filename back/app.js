3933569ec20ebe5038315bc5174f794bcce783ea
ChokoDev <jphilblanchet@yahoo.fr>
2025-06-02 17:12:22 +0200

    setup server

---
+++ back/app.js
@@ -0,0 +1,12 @@
+// salut
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("hello"));
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log("Salut ^^")
+})
