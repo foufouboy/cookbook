82efc4c6a394cbeed4df4a7acc1cd085b096f11d
ThomasDev31 <thomas.tissier@3wa.io>
2025-06-02 16:52:49 +0200

    adding pages (front)

--- front/src/components/header.jsx
+++ front/src/components/header.jsx
@@ -1,9 +1,15 @@
 import '../styles/header.css';
-
+import { NavLink } from 'react-router';
 function Header() {
 
   return (
     <>
+    <div className='navLink'>
+      <NavLink  to='/' > Accueil</NavLink>
+      <NavLink  to='/Login' > Connexion</NavLink>
+      <NavLink  to='/Register' > Enregistrement</NavLink>
+      <NavLink  to='/Logout' > Déconnexion</NavLink>
+    </div>
     </>
   )
 }
