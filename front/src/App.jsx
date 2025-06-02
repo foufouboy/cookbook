ccec7545246990dfa3dda354d510bd45752e7a9a
Clement-Robbe <clement.robbe@gmail.com>
2025-06-02 16:46:37 +0200

    123

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,24 @@
-import './App.css';
-import Home from './pages/Home'
-
+import "./styles/App.sass";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-
-  return (
-    <>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/Register" element={<Register />} />
+                <Route path="/*" element={<Error />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
