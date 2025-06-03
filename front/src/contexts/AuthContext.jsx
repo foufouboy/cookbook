4f0ec6ff2a4ffa0c5ca18e00f8e0b294c513a89c
Clement-Robbe <clement.robbe@gmail.com>
2025-06-03 11:26:16 +0200

    authcontext

--- front/src/contexts/AuthContext.jsx
+++ front/src/contexts/AuthContext.jsx
@@ -1,3 +1,64 @@
-function AuthContext() {}
+import { createContext, useContext } from "react";
 
-export default AuthContext;
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [token, setToken] = useState(localStorage.getItem("token"));
+    const [isAdmin, setIsAdmin] = useState(false);
+
+    async function checkAdminStatus() {
+        if (token) {
+            try {
+                const response = await fetch("http://localhost:3000/user", {
+                    method: "GET",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                if (response.ok) {
+                    const data = await response.json();
+                    setIsAdmin(data.isAdmin);
+                } else {
+                    setIsAdmin(false);
+                }
+            } catch (error) {
+                console.error("Error checking admin status:", error);
+                setIsAdmin(false);
+            }
+        } else {
+            setIsAdmin(false);
+        }
+    }
+
+    const setBothTokens = (newToken) => {
+        localStorage.setItem("token", newToken);
+        setToken(newToken);
+    };
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        setToken(null);
+    };
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem("token");
+        if (storedToken !== token) {
+            setToken(storedToken);
+        }
+        checkAdminStatus();
+    }, [token]);
+
+    return (
+        <AuthContext.Provider value={{ token, setBothTokens, logout, isAdmin }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuthContext = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuthContext must be used within an AuthProvider");
+    }
+    return context;
+};
