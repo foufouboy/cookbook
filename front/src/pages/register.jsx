9b946ac6e3d12b3683bd4f8a80a0d651d44a84ea
Clement-Robbe <clement.robbe@gmail.com>
2025-06-03 11:31:41 +0200

    fix

--- front/src/pages/register.jsx
+++ front/src/pages/register.jsx
@@ -1,7 +1,34 @@
 import "../styles/register.sass";
 
 function Register() {
-    return <></>;
+    async function register(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData.entries());
+
+        try {
+            const response = await fetch("http://localhost:3000/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            const result = await response.json();
+            console.log("Registration successful:", result);
+        } catch (error) {
+            console.error("Error during registration:", error);
+        }
+    }
+    return (
+        <div className="Register">
+            <h1>Inscription</h1>
+            <form onSubmit={register}></form>
+        </div>
+    );
 }
 
 export default Register;
