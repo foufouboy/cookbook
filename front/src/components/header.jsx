739b213a8d23ac2dac753122793cdb74efb231c4
ThomasDev31 <thomas.tissier@3wa.io>
2025-06-03 10:36:14 +0200

    Styled footer/header and adding page error404

--- front/src/components/header.jsx
+++ front/src/components/header.jsx
@@ -1,18 +1,20 @@
 import "../styles/header.sass";
 import { NavLink } from "react-router";
+
 function Header() {
     return (
         <>
             <div className="navLink">
                 <div className="logo">
-                  <NavLink to="/"> <img src="logo.png" /> </NavLink>
+                    <NavLink to="/">
+                        <img src="logo.png" />
+                    </NavLink>
                 </div>
                 <div className="links">
-                  <NavLink to="/"> Accueil</NavLink>
-                  <NavLink to="/Login"> Connexion</NavLink>
-                  <NavLink to="/Register"> Enregistrement</NavLink>
-                  <NavLink to="/Logout"> Déconnexion</NavLink>
-
+                    <NavLink to="/"> Accueil</NavLink>
+                    <NavLink to="/Login"> Connexion</NavLink>
+                    <NavLink to="/Register"> Enregistrement</NavLink>
+                    <NavLink to="/Logout"> Déconnexion</NavLink>
                 </div>
             </div>
         </>
--- front/src/pages/Error404.jsx
+++ front/src/pages/Error404.jsx
@@ -1,7 +1,13 @@
 import "../styles/Error404.sass";
-
+import { Link } from "react-router";
 function Error404() {
-    return <></>;
+    return <>
+        <div className="container-error">
+            <h1>404</h1>
+            <h2>Pages introuvables</h2>
+            <Link to={'/'}>retour à l'accueil</Link>
+        </div>
+    </>;
 }
 
 export default Error404;
