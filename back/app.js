743aaed6c77522b193a8f806b44353caf4c046c6
ThomasDev31 <thomas.tissier@3wa.io>
2025-06-06 16:02:28 +0200

    Changing app.js

--- back/app.js
+++ back/app.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const path = require('path');
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
+const path = require('path');
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+
+app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
 
 const userRoutes = require("./routes/userRoutes.js");
 const recipeRoutes = require("./routes/recipeRoutes.js");
