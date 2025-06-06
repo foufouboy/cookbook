ab78d3292759d2fa12e6ed4bfe8943ad9a0776c9
KalmGuy <raphael.gregori.gd@gmail.com>
2025-06-06 16:19:32 +0200

    ALED

--- back/routes/userRoutes.js
+++ back/routes/userRoutes.js
@@ -1,14 +1,21 @@
 const userRoutes = require("express").Router();
+
 const userController = require("../controllers/userController.js");
+
+
 const { validatedUser } = require("../middlewares/validator.js");
 // HOME
+
 userRoutes.get("/", (req, res) => res.redirect("recipes"));
+
 
 // LOGIN & REGISTER
 userRoutes.post("/auth/login", userController.login);
+
 userRoutes.post("/auth/logout", userController.logout);
 
 userRoutes.post("/auth/register", validatedUser, userController.register);
+
 
 module.exports = userRoutes;
 //ALED GIT
